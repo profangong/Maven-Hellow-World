d8650288e27f93a6d6d3efd039d0267bd3c995cc
profangong <profangong@163.com>
2019-06-23 13:30:05 +0800

    first commit

---
+++ src/main/java/Maven/Hellow/World/App.java
@@ -0,0 +1,10 @@
+package Maven.Hellow.World;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("something happen.");
+	}
+
+}
